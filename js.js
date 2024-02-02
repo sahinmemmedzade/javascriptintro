59e0cb2484abaeffdcfe2e75db721e6a64210816
sahinmemmedzade <shahinim@code.edu.az>
2024-02-02 05:05:19 +0400

    jsavascript asand muellim bir az cetin verin de nolar

---
+++ js.js
@@ -0,0 +1,121 @@
+// 4 reqemli eded verilib. Bu ededin eveline 7 reqemini , axirinada 8 reqemini artir. Example1: 3333= 733338
+// var eded=3333;
+// var c="";
+// var arr=[];
+// while(eded>=1){
+//     var d=eded%10;
+// eded=(eded-eded%10)/10;
+// arr.push[d];
+// for(i=arr.length;i>=0;i--){
+//     c=c+d;
+// }
+// }
+// var d=parseInt(c);
+// var num2=d*10+700000+8
+// console.log(num2);
+//2) 1-1000 qeder ederlerin icerisinden ele ededleri cap et ki, reqemleri cemi 3-e bolunsun
+
+// var sum=0;
+// for(i=0;i<=1000;i++){
+// while(i>=1){
+//     var a=i%10;
+//     a=Math.floor(a);
+// i=(i-i%10)/10;
+//     sum=sum+a
+// }
+// if(sum%3==0){
+//     console.log(sum)
+// }
+// }
+// 2 dene eded verilib. I eded 4 reqemli II eded 7 reqemlidir.
+// I ededin 4%-ni tap. 
+// Sonra II ededin 9% ni tap. 
+// Sonra Cavalari toplayib 10 %ni tap.
+// var a=1000;
+// var b=1000000;
+// console.log((a*0.04+b*0.09)*0.1);
+// Qapi var hundurluk ve eni verilib.Elimizde skaf var uzunlugu hundurluyu ve eni verilib.Bu skaf qapidan kecermi?
+// var qapien=4;
+// var qapihund=5;
+// var skafhund=20;
+// var skafen=3;
+// var skafuzun=3;
+// if(qapien<skafhund || qapien<skafen || qapien<skafuzun){
+//     console.log("kecmez");
+// }
+// else if(qapihund<skafhund || qapihund<skafen||qapihund<skafuzun){
+//    console.log("kecmez");
+// }
+
+
+// else{
+//     console.log("kecer")
+
+// Qapi var hundurluk ve eni verilib.Elimizde skaf var uzunlugu hundurluyu ve eni verilib.Bu skaf qapidan kecermi?5)
+
+// SWITCH operatoru ile yoxla.
+// Verilmish ededdin sonuncu reqemi
+               
+//      1 olarsa-"I gun" sozunu
+              
+//      2 olarsa-"II gun" sozunu
+  
+//                     ...
+               
+//      7 olarsa-"VI gun" sozunu yaz
+// var eded=1231;
+//  var d=eded%10;
+//  switch (d) {
+//     case 1:
+//         console.log("1 ci gun")
+//         break;
+//         case 2:
+//             console.log("2 ci gun")
+//             break;
+//             case 3:
+//             console.log("3 cu gun")
+//             break;
+//             case 4:
+//             console.log("4 cu gun")
+//             break;
+//             case 5:
+//             console.log("5 ci gun")
+//             break;
+//             case 6:
+//             console.log(" 6 ci gun")
+//             break;
+//             case 7:
+//             console.log("7 ci gun")
+//             break;
+//     default:
+//         console.log("diger ededdir");
+//         break;
+//  }
+
+
+// 6)IF operatoru ile yoxla.
+// 6 dene eded verilib. Bu ededlerden hansi en boyukdur.
+// var a =55;
+// var b =50;
+// var c =85;
+// var d= 92;
+// var e=202;
+// var f= 700;
+// if(a>b && a>c && a>d && a>e && a>f){
+//     console.log("a en boyukdur")
+// }
+// else if( b>c && b>d && b>e && b>f){
+//     console.log("b en boyukdur")
+// }
+// else if( c>d && c>e && c>f){
+//     console.log("c en boyukdur")
+// }
+// else if(  d>e && d>f){
+//     console.log("d en boyukdur")
+// }
+// else if( e>f){
+//     console.log("e en boyukdur")
+// }
+// else{
+//     console.log("f en boyukdur")
+// }
